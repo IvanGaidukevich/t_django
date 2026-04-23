b27500665456413b5868680be707161badbbb670
vania_delay <gaydu7@gmail.com>
2026-04-23 14:56:20 +0300

    accounts add

--- my_store/accounts/admin.py
+++ my_store/accounts/admin.py
@@ -2,9 +2,3 @@ from django.contrib import admin
 
 from accounts.models import User
 
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined']
-    list_filter = ['is_active', 'is_staff', 'date_joined']
-    search_fields = ['username', 'first_name', 'last_name', 'email']
