34eb606f2ee1ce681d075fa3f4c140aed84054db
vania_delay <gaydu7@gmail.com>
2026-05-12 15:20:36 +0300

    cart add

--- my_store/cart/views.py
+++ my_store/cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from catalog.models import Product
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
 
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('catalog:product_list')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    pass
+
+
+def cart_detail(request):
+    pass
 # Create your views here.
